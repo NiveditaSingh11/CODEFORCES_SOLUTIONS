4c1183d4fe2f4fac3d0e4d3cb205f6bd64772f9a
Nivedita Singh <niveditareenasingh@gmail.com>
2026-06-16 19:56:29 +0530

    Add Codeforces solutions

---
+++ 281A_Word_Capitalisation.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+
+    for(int i=0;i<1;i++){
+        if(isupper(s[0])){
+            break;
+            
+        }
+        else{
+            s[0] =toupper(s[0]);
+        }
+    }
+    cout<<s<<endl;
+    return 0;
+}
---
+++ 58A_Chat_Room.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin >>s;
+   string target = "hello";
+   int j=0;
+    for(int i=0;i<s.length();i++){
+        if(j<5 && s[i]==target[j]){
+            j++;
+        }
+    }
+    cout<<(j == 5? "yes":"no");
+    return 0;
+}
