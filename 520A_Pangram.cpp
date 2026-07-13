8c76abd7ffba1278a79e1834faa8be4f16baf472
Nivedita Singh <niveditareenasingh@gmail.com>
2026-07-13 22:00:51 +0530

    solutions

---
+++ 520A_Pangram.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    string s;
+    cin>>s;
+
+    set<char> letters;
+
+    for (char ch:s){
+        letters.insert(tolower(ch));
+    }
+    if(letters.size()==26){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
